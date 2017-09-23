959aa133ea93999c670143fa85d94f6568493080
Karlo Kolar <alien@kolar.link>
2017-09-23 14:30:01 +0200

    Edit and update added

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -25,7 +25,14 @@ class PortfoliosController < ApplicationController
   end
 
   def update
-    #code
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
 
