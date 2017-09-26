2e79bced00ece6a758fdbe2e101cb29bc3826fba
Karlo Kolar <alien@kolar.link>
2017-09-26 06:48:25 +0200

    integrated nested attr for portfolio model

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,7 +42,16 @@ end
   body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
   main_image: "http://via.placeholder.com/600x400",
   thumb_image: "http://via.placeholder.com/350x200"
-)
+  )
 end
 
 puts "9 portfolios"
+
+3.times do |t|
+  Technology.create!(
+    name: "Technology #{t}",
+    portfolio_id: Portfolio.last.id
+  )
+end
+
+puts "3 technologies"
